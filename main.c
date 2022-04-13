3c5adf15b1c33123bf882fc51e6a5d61597eec4b
blzby1 <96076743+blzby1@users.noreply.github.com>
2022-04-13 10:22:32 -0500

    Create main.c
    hi

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+  // some very, very complicated code  (FOR PROFESSIONALS ONLY1!1!!!!!)
+  for (int i = 99; i > 0; i--) {
+    printf("%d bottles of pop on the wall.\n", i);
+    printf("%d bottles of pop! \n", i);
+    printf("Take one down and pass it around.\n");
+    printf("%d bottles of pop on the wall.\n", i - 1);
+  }
+
+}
