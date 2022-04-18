9f177b3177ea586bc172c7638856fd8d50cdcb60
blzby1 <96076743+blzby1@users.noreply.github.com>
2022-04-18 16:13:33 -0500

    Remove an unnecessary space :)

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main() {
   // some very, very complicated code  (FOR PROFESSIONALS ONLY1!1!!!!!)
   for (int i = 99; i > 0; i--) {
     printf("%d bottles of pop on the wall.\n", i);
-    printf("%d bottles of pop! \n", i);
+    printf("%d bottles of pop!\n", i);
     printf("Take one down and pass it around.\n");
     printf("%d bottles of pop on the wall.\n", i - 1);
   }
